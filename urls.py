fcdc8fd27b6c92d7c9824eb555c46d8c33726f8e
shivaniKajbe <96592793+shivaniKajbe@users.noreply.github.com>
2023-04-17 17:22:12 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,32 @@
+"""suma URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from soft.views import *
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',index,name='index'),
+    path('login/',login,name='login'),
+    path('add_user/',add_user,name='add_user'),
+    path('add_new_profile/',add_new_profile,name='add_new_profile'),
+    path('index/', back, name='index'),
+    path('show_all_profile/',show_all_profile,name='show_all_profile'),
+    path('single_profile/<int:profile_id>/', single_profile, name='single_profile'),
+    path('update_profile/<int:profile_id>/', update_profile, name='update_profile'),
+
+]
